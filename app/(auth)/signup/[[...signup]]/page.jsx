050530fba9645d8e86c7e554f8846a40986f098a
cjrojas72 <cjrojas72@gmail.com>
2024-06-08 14:22:50 -0400

    initial commit of nxt-finance

--- app/(auth)/signup/[[...signup]]/page.jsx
+++ app/(auth)/signup/[[...signup]]/page.jsx
@@ -1,8 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return(
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-1">
+    return(
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-1">
             <div className="h-full lg:flex flex-col items-center justify center px-4">
                 <div className="text-center space-y-4 pt-16">
                     <h1 className="font-bold text-3xl">NXTJS Finance</h1>
